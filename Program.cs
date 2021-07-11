ed83f4e68b01051ec140e8674eca959fdd713c4e
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-07-11 20:21:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BA_11_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
